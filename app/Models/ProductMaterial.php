96eedb46873409fc5a6b692f8c4ca818d8c27a25
Muhammad Rachel Fathan Idzhany <racheltambunan10@gmail.com>
2026-05-14 15:04:14 +0700

    add Material, Product, and StockLog models with relationships; create Filament resources and forms for managing materials, products, and stock logs

--- app/Models/ProductMaterial.php
+++ app/Models/ProductMaterial.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ProductMaterial extends Model
+class ProductMaterial extends Pivot
 {
-    //
+    protected $table = 'product_material';
+
+    protected $fillable = [
+        'product_id',
+        'material_id',
+        'quantity_needed',
+    ];
 }
