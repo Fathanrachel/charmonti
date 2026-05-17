cd29aa4334d6701c3dd6a42e94d807249fd23112
Muhammad Rachel Fathan Idzhany <racheltambunan10@gmail.com>
2026-05-17 15:35:36 +0700

    feat: Implement policy classes for financial, order, payment, product, and sales reports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,32 @@
 
 namespace App\Providers;
 
+use App\Models\FinancialReport;
 use App\Models\Order;
+use App\Models\Payment;
+use App\Models\Product;
+use App\Models\SalesReport;
 use App\Observers\OrderObserver;
-use Illuminate\Support\ServiceProvider;
+use App\Policies\FinancialReportPolicy;
+use App\Policies\OrderPolicy;
+use App\Policies\PaymentPolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\SalesReportPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends AuthServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    protected $policies = [
+        Product::class        => ProductPolicy::class,
+        Order::class          => OrderPolicy::class,
+        Payment::class        => PaymentPolicy::class,
+        FinancialReport::class => FinancialReportPolicy::class,
+        SalesReport::class    => SalesReportPolicy::class,
+    ];
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        $this->registerPolicies();
         Order::observe(OrderObserver::class);
     }
 }
